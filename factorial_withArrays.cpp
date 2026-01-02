f2fcd5702a0175db1b57ed621cfaf4f525cc8b3f
007Rizan <mavisigece26@gmail.com>
2026-01-02 18:19:47 +0300

    Implement factorial using arrays

---
+++ factorial_withArrays.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<array>
+using namespace std;
+
+int productArray(array<int,6>myArray);
+
+
+int main() {
+	int total = productArray({ 1,2,3,4,5,6 });
+
+	cout << "The product of the array elements is: " << total << endl;
+
+}
+
+int productArray(array<int,6>myArray) {
+	int product = 1;
+	for (int i = 0; i < myArray.size(); i++) {
+		product *= myArray[i];
+	}
+	return product;
+}
