2cb9fdc1bc9f235cf22b23ac60552f66beb870f7
007Rizan <mavisigece26@gmail.com>
2026-03-10 00:15:59 +0300

    Implement Dog class with breed and digging behavior

---
+++ Animal questioning program(Simple)/Dog.cpp
@@ -0,0 +1,18 @@
+#include "Dog.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+Dog::Dog(string name, double weight, string breed) :Animal(name,weight){
+	this->breed = breed;
+}
+
+string Dog::getBreed() const {
+	return breed;
+}
+void Dog::digHole() const{
+	cout << "Digging a hole!" << endl;
+
+}
