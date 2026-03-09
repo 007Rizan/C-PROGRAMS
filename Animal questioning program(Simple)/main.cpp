e177653aefaa2c3e25624ccb3f218f39289ba2b1
007Rizan <mavisigece26@gmail.com>
2026-03-10 00:16:34 +0300

    Include Dog class functionality in main program

--- Animal questioning program(Simple)/main.cpp
+++ Animal questioning program(Simple)/main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include "Animal.h"
+#include "Dog.h"
 using namespace std;
 
 
 int main() {
 	Animal myAnimal("Bob", 10.5);
-
+	Dog dog("Rex", 20, "Labrador");
 	cout << "Animal Name: " << myAnimal.getName() << endl;
 	cout << "Animal Weight: " << myAnimal.getWeight() << " kg" << endl;
 	cout << "Animal Noise: " << myAnimal.makeNoise() << endl;
+
+	cout << "Animal Name: " << dog.getName() << endl;
+	cout << "Animal Weight: " << dog.getWeight() << " kg" << endl;
+	cout << "Animal Noise: " << dog.makeNoise() << endl;
+
+	
+	dog.digHole();
+	return 0;
 }
